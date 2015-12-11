13a0c42a4dd6ae66881da20c4169112aafdb5aa7
Kyle Hall <hallkd47@gmail.com>
2015-12-11 06:42:18 -0600

    adds solution to euler problem 1

--- web-dev-101/javascript/euler/problem1.js
+++ web-dev-101/javascript/euler/problem1.js
@@ -1,5 +1,13 @@
 
 function threesAndFives (upperBound) {
   //find all the multiples of 3 and 5 less than upperBound and sum them
-
+  var sum = 8;
+  for (var i = 6; i < upperBound; i++) {
+    if (i % 3 == 0) {
+      sum += i;
+    } else if (i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
 }
